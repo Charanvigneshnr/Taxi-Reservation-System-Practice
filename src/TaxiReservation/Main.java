96a3ba66796fafe7846d75b0fbe12903cd68ca80
Charanvigneshnr <charanvignesh10@gmail.com>
2023-10-28 16:00:07 +0530

    Initial commit

---
+++ src/TaxiReservation/Main.java
@@ -0,0 +1,8 @@
+package TaxiReservation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Taxi Reservation System");
+        
+    }
+}
---
+++ src/TaxiReservation/Reservation.java
@@ -0,0 +1,4 @@
+package TaxiReservation;
+
+public class Reservation {
+}
